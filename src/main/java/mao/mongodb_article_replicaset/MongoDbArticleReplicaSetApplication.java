6ec1ce231c9b2401d4a52e5e8c2e819ca9a7fc03
mao <1296193245@qq.com>
2022-11-18 13:32:28 +0800

    MongoDB_article_replicaSet

---
+++ src/main/java/mao/mongodb_article_replicaset/MongoDbArticleReplicaSetApplication.java
@@ -0,0 +1,15 @@
+package mao.mongodb_article_replicaset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbArticleReplicaSetApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MongoDbArticleReplicaSetApplication.class, args);
+    }
+
+}
